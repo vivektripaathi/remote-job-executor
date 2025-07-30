be5e221d621d3ac443efaaa31be79fc12770213c
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-07-30 21:40:52 +0530

    feat: implement dependency injection.

--- backend/jobs/apps.py
+++ backend/jobs/apps.py
@@ -4,3 +4,35 @@ from django.apps import AppConfig
 class JobsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'jobs'
+    inject_container = None
+
+    def ready(self):
+        from jobs.inject import JobContainer
+        import jobs.presentation.views
+        from jobs.domain.use_cases import (
+            create_job_use_case,
+            get_job_use_case,
+            list_jobs_use_case,
+            update_job_use_case,
+            cancel_job_use_case,
+            delete_job_use_case,
+            execute_remote_command_use_case,
+            execute_remote_command_streaming_use_case,
+            kill_remote_process_use_case,
+        )
+
+        self.inject_container = JobContainer()
+        self.inject_container.wire(
+            modules=[
+                jobs.presentation.views,
+                create_job_use_case,
+                get_job_use_case,
+                list_jobs_use_case,
+                update_job_use_case,
+                cancel_job_use_case,
+                delete_job_use_case,
+                execute_remote_command_use_case,
+                execute_remote_command_streaming_use_case,
+                kill_remote_process_use_case,
+            ],
+        )
---
+++ backend/jobs/inject.py
@@ -0,0 +1,44 @@
+from dependency_injector import containers, providers
+
+from jobs.data.abstract_repo import JobAbstractRepository
+from jobs.data.db_repo import JobDbRepository
+from jobs.infrastructure.ssh_client import SSHClientInterface, SSHClient
+from jobs.infrastructure.remote_command_executor import RemoteCommandExecutorInterface, RemoteCommandExecutor
+from jobs.domain.use_cases.create_job_use_case import CreateJobUseCase
+from jobs.domain.use_cases.get_job_use_case import GetJobUseCase
+from jobs.domain.use_cases.list_jobs_use_case import ListJobsUseCase
+from jobs.domain.use_cases.update_job_use_case import UpdateJobUseCase
+from jobs.domain.use_cases.cancel_job_use_case import CancelJobUseCase
+from jobs.domain.use_cases.delete_job_use_case import DeleteJobUseCase
+from jobs.domain.use_cases.execute_remote_command_use_case import ExecuteRemoteCommandUseCase
+from jobs.domain.use_cases.execute_remote_command_streaming_use_case import ExecuteRemoteCommandStreamingUseCase
+from jobs.domain.use_cases.kill_remote_process_use_case import KillRemoteProcessUseCase
+
+
+class JobContainer(containers.DeclarativeContainer):
+    # Infrastructure
+    ssh_client = providers.Dependency(
+        instance_of=SSHClientInterface,
+        default=SSHClient(),
+    )
+    
+    remote_executor = providers.Dependency(
+        instance_of=RemoteCommandExecutorInterface,
+        default=RemoteCommandExecutor(),
+    )
+    
+    db_repo = providers.Dependency(
+        instance_of=JobAbstractRepository,
+        default=JobDbRepository(),
+    )
+    
+    # Use cases
+    create_job_use_case = providers.Factory(CreateJobUseCase)
+    get_job_use_case = providers.Factory(GetJobUseCase)
+    list_jobs_use_case = providers.Factory(ListJobsUseCase)
+    update_job_use_case = providers.Factory(UpdateJobUseCase)
+    cancel_job_use_case = providers.Factory(CancelJobUseCase)
+    delete_job_use_case = providers.Factory(DeleteJobUseCase)
+    execute_remote_command_use_case = providers.Factory(ExecuteRemoteCommandUseCase)
+    execute_remote_command_streaming_use_case = providers.Factory(ExecuteRemoteCommandStreamingUseCase)
+    kill_remote_process_use_case = providers.Factory(KillRemoteProcessUseCase)
